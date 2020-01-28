848e13783283bc868bdf91b40617fe97a3ae17ed
Supachai Chaisongkhram <mhor.fibo7@gmail.com>
2020-01-28 14:15:31 +0700

    First Commit for client application

---
+++ WindowsFormsSample/ChatForm.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace WindowsFormsSample
+{
+    partial class ChatForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.messagesList = new System.Windows.Forms.ListBox();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.messageTextBox = new System.Windows.Forms.TextBox();
+            this.disconnectButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsers = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.addressTextBox.Location = new System.Drawing.Point(78, 10);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(483, 20);
+            this.addressTextBox.TabIndex = 0;
+            this.addressTextBox.Enter += new System.EventHandler(this.addressTextBox_Enter);
+            // 
+            // connectButton
+            // 
+            this.connectButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.connectButton.Location = new System.Drawing.Point(593, 8);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(75, 23);
+            this.connectButton.TabIndex = 1;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Address:";
+            // 
+            // messagesList
+            // 
+            this.messagesList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.messagesList.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.messagesList.FormattingEnabled = true;
+            this.messagesList.Location = new System.Drawing.Point(15, 127);
+            this.messagesList.Name = "messagesList";
+            this.messagesList.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.messagesList.Size = new System.Drawing.Size(734, 394);
+            this.messagesList.TabIndex = 3;
+            this.messagesList.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.messagesList_DrawItem);
+            // 
+            // sendButton
+            // 
+            this.sendButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.sendButton.Enabled = false;
+            this.sendButton.Location = new System.Drawing.Point(674, 532);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(75, 23);
+            this.sendButton.TabIndex = 5;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.messageTextBox.Enabled = false;
+            this.messageTextBox.Location = new System.Drawing.Point(15, 534);
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.Size = new System.Drawing.Size(653, 20);
+            this.messageTextBox.TabIndex = 4;
+            this.messageTextBox.Enter += new System.EventHandler(this.messageTextBox_Enter);
+            // 
+            // disconnectButton
+            // 
+            this.disconnectButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.disconnectButton.Enabled = false;
+            this.disconnectButton.Location = new System.Drawing.Point(674, 8);
+            this.disconnectButton.Name = "disconnectButton";
+            this.disconnectButton.Size = new System.Drawing.Size(75, 23);
+            this.disconnectButton.TabIndex = 6;
+            this.disconnectButton.Text = "Disconnect";
+            this.disconnectButton.UseVisualStyleBackColor = true;
+            this.disconnectButton.Click += new System.EventHandler(this.disconnectButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Users:";
+            // 
+            // txtUsers
+            // 
+            this.txtUsers.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUsers.Location = new System.Drawing.Point(78, 39);
+            this.txtUsers.Name = "txtUsers";
+            this.txtUsers.Size = new System.Drawing.Size(483, 20);
+            this.txtUsers.TabIndex = 7;
+            this.txtUsers.Text = "mhor.fibo7@gmail.com";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Password:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPassword.Location = new System.Drawing.Point(78, 68);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(483, 20);
+            this.txtPassword.TabIndex = 9;
+            this.txtPassword.Text = "Mhor.fibo7";
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLogin.Location = new System.Drawing.Point(593, 42);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 46);
+            this.btnLogin.TabIndex = 11;
+            this.btnLogin.Text = "Log In";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLogout.Location = new System.Drawing.Point(674, 42);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(75, 46);
+            this.btnLogout.TabIndex = 12;
+            this.btnLogout.Text = "Log Out";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            // 
+            // ChatForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 566);
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUsers);
+            this.Controls.Add(this.disconnectButton);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.messageTextBox);
+            this.Controls.Add(this.messagesList);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.addressTextBox);
+            this.Name = "ChatForm";
+            this.Text = "SignalR Chat Sample";
+            this.Load += new System.EventHandler(this.ChatForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox messagesList;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.TextBox messageTextBox;
+        private System.Windows.Forms.Button disconnectButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUsers;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnLogout;
+    }
+}
+
---
+++ WindowsFormsSample/ChatForm.cs
@@ -0,0 +1,229 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Security;
+using System.Windows.Forms;
+using Microsoft.AspNetCore.SignalR.Client;
+using System.IdentityModel;
+using System.Security;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.SqlClient;
+namespace WindowsFormsSample
+{
+    public partial class ChatForm : Form
+    {
+        private HubConnection _connection;
+
+        public ChatForm()
+        {
+            InitializeComponent();
+        }
+        
+        private void ChatForm_Load(object sender, EventArgs e)
+        {
+            addressTextBox.Focus();
+        }
+
+        private void addressTextBox_Enter(object sender, EventArgs e)
+        {
+            AcceptButton = connectButton;
+        }
+        private async void connectButton_Click(object sender, EventArgs e)
+        {
+            UpdateState(connected: false);
+            var token = getToken();
+            _connection = new HubConnectionBuilder()
+                .WithAutomaticReconnect()
+                .WithUrl("https://localhost:44377/ChatHub",options =>
+                {
+                    options.AccessTokenProvider = () => Task.FromResult(token);
+                })
+                .Build();
+           //_connection.On<string, string>("broadcastMessage", (s1, s2) => OnSend(s1, s2));
+
+            Log(Color.Gray, "Starting connection...");
+            try
+            {
+                await _connection.StartAsync();
+            }
+            catch (Exception ex)
+            {
+                Log(Color.Red, ex.ToString());
+                return;
+            }
+
+            Log(Color.Gray, "Connection established.");
+
+            UpdateState(connected: true);
+
+            messageTextBox.Focus();
+        }
+
+        private async void disconnectButton_Click(object sender, EventArgs e)
+        {
+            Log(Color.Gray, "Stopping connection...");
+            try
+            {
+                await _connection.StopAsync();
+            }
+            catch (Exception ex)
+            {
+                Log(Color.Red, ex.ToString());
+            }
+
+            Log(Color.Gray, "Connection terminated.");
+
+            UpdateState(connected: false);
+        }
+
+        private void messageTextBox_Enter(object sender, EventArgs e)
+        {
+            AcceptButton = sendButton;
+        }
+
+        private async void sendButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //await _connection.InvokeAsync("Send", "WinFormsApp", messageTextBox.Text);
+                await _connection.InvokeAsync("SendMessage", messageTextBox.Text, "");
+            }
+            catch (Exception ex)
+            {
+                Log(Color.Red, ex.ToString());
+            }
+        }
+
+        private void UpdateState(bool connected)
+        {
+            disconnectButton.Enabled = connected;
+            connectButton.Enabled = !connected;
+            addressTextBox.Enabled = !connected;
+
+            messageTextBox.Enabled = connected;
+            sendButton.Enabled = connected;
+        }
+
+        private void OnSend(string name, string message)
+        {
+            Log(Color.Black, name + ": " + message);
+        }
+
+        private void Log(Color color, string message)
+        {
+            Action callback = () =>
+            {
+                messagesList.Items.Add(new LogMessage(color, message));
+            };
+
+            Invoke(callback);
+        }
+
+        private class LogMessage
+        {
+            public Color MessageColor { get; }
+
+            public string Content { get; }
+
+            public LogMessage(Color messageColor, string content)
+            {
+                MessageColor = messageColor;
+                Content = content;
+            }
+        }
+
+        private void messagesList_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            var message = (LogMessage)messagesList.Items[e.Index];
+            e.Graphics.DrawString(
+                message.Content,
+                messagesList.Font,
+                new SolidBrush(message.MessageColor),
+                e.Bounds);
+        }
+
+        public string getToken()
+        {
+            var userName = txtUsers.Text;
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Sub,userName),
+                new Claim(JwtRegisteredClaimNames.Email,userName),
+                new Claim(ClaimTypes.Role,"Machine"),
+                new Claim("FactoryName","Tanjai Delivery")
+                //new Claim(JwtRegisteredClaimNames.NameId,userName),
+                //new Claim("my key","my value"),
+            };
+            
+            // Define const Key this should be private secret key  stored in some safe place
+            string key = "secret-keymhor";
+
+           // Create Security key  using private key above:
+           // not that latest version of JWT using Microsoft namespace instead of System
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF32.GetBytes(key));
+
+            // Also note that securityKey length should be >256b
+            // so you have to make sure that your private key has a proper length
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var secToken = new JwtSecurityToken(
+                issuer: "https://localhost:44377/",
+                audience: "https://localhost:44377/",
+                claims: claims,
+                notBefore: DateTime.UtcNow,
+                expires:DateTime.Now.AddMonths(3),
+                signingCredentials: credentials
+                );
+
+
+            var handler = new JwtSecurityTokenHandler();
+
+            // Token to String so you can use it in your client
+            var tokenString = handler.WriteToken(secToken);
+
+            return tokenString;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            
+            //try
+            //{
+            //    var t = VerifyUserNamePassword(u, p);
+            //    MessageBox.Show(t.ToString());
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+            //}
+            //// MessageBox.Show(getToken());
+
+
+            //try
+            //{
+            //    var t = CreateUser(u, p);
+            //    var errores = "";
+            //    t.Errors.ToList().ForEach(x => errores += x + Environment.NewLine);
+            //    MessageBox.Show(t.ToString() + errores);
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+            //}
+
+        }
+       
+
+    }
+}
